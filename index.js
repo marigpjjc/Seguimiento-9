5450c9c7812a7ef5a2ade0f18bfa7f95c5dbd350
marigpjjc <galvis.5.mgp@gmail.com>
2024-09-28 17:37:41 -0500

    Chat personal

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ function sendMessage(event, user) {
     const userInput = user === 'user1' ? document.getElementById('usermsg1') : document.getElementById('usermsg2');
     const messageText = userInput.value.trim();
 
-    if (messageText) {
-        
-        const newMessage = document.createElement('div');
-        newMessage.className = user === 'user1' ? 'user1-msg' : 'user2-msg'; // Cambia según el usuario
-        newMessage.textContent = messageText;
-
-        const chatbox = document.getElementById('chatbox');
-        chatbox.appendChild(newMessage);
-
-        userInput.value = '';
-
-        chatbox.scrollTop = chatbox.scrollHeight;
+    if (messageText === "") {
+        alert("Por favor, ingresa un mensaje antes de enviar."); 
+        return; 
     }
+
+    const newMessage = document.createElement('div');
+    newMessage.className = user === 'user1' ? 'user1-msg' : 'user2-msg'; 
+    newMessage.textContent = messageText;
+
+    const chatbox = document.getElementById('chatbox');
+    chatbox.appendChild(newMessage);
+
+    userInput.value = '';
+
+    chatbox.scrollTop = chatbox.scrollHeight; 
 }
